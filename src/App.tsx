060eec3eb1ba00b883a4aca5d6ce950eaa3f7c44
Joseph Fuge <josephfuge1@gmail.com>
2024-03-04 16:07:27 -0700

    Do mission 9 things

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import teamsData from './CollegeBasketballTeams.json';
+
+interface TeamProps {
+  tid: number;
+  cid: number;
+  did: number;
+  school: string;
+  name: string;
+  abbrev: string;
+  pop: number;
+  city: string;
+  state: string;
+  latitude: number;
+  longitude: number;
+}
+
+function Heading() {
+  return (
+    <div className="Heading">
+      <h2>NCAA Basketball Info</h2>
+      <p className="HeadingText">
+        This site provides information about the college teams that participate
+        in NCAA Basketball in order to help you fill out the best March Madness
+        bracket you can.
+      </p>
+    </div>
+  );
+}
+
+// {"tid": 159, "cid": 0, "did": 0, "school": "Boston College", "name": "Eagles", "abbrev": "BC", "pop": 161, "city": "Chestnut Hill", "state": "MA", "latitude": 42.330, "longitude": -71.166},
+function TeamCard({ ...TeamProps }) {
+  return (
+    <div className="TeamCard">
+      <h3>School: {TeamProps.school}</h3>
+      <p>Mascot Name: {TeamProps['name']}</p>
+      <p>
+        Location: {TeamProps['city']}, {TeamProps['state']}
+      </p>
+    </div>
+  );
+}
+
+function TeamList() {
+  return (
+    <div className="TeamList">
+      {teamsData['teams'].map((teamInfo, index) => (
+        <TeamCard {...{ key: index, ...teamInfo }} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Heading />
       </header>
+      <TeamList />
     </div>
   );
 }
